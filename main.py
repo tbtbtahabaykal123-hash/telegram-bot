cf6f8cb3dd046adb8f7a1946ac2cea4c3d85d735
tbtbtahabaykal123-hash <tb.tb.tahabaykal123@gmail.com>
2026-08-20 09:14:58 +0300

    Update main.py

--- main.py
+++ main.py
@@ -5,29 +5,34 @@ from threading import Thread
 from flask import Flask
 from telegram import Bot
 
-# Render Port Taramasını Geçmek İçin Web Sunucusu
+# Render Web Sunucusu
 app = Flask('')
+
 
 @app.route('/')
 def home():
   return 'Bot Calisiyor!'
 
+
 def run():
   port = int(os.environ.get('PORT', 8080))
   app.run(host='0.0.0.0', port=port)
 
+
 def keep_alive():
   t = Thread(target=run)
+  t.daemon = True
   t.start()
+
 
 # TELEGRAM BOT KODLARI
 TOKEN = '8978792663:AAFEmc5qriY8a4tuX05yxzARfEr5KgwIMM0'
 CHAT_ID = -1002247545043
-MESSAGE_ID = 23  # Düzeltildi: 23 olarak ayarlandı
+MESSAGE_ID = 23
 
-bot = Bot(token=TOKEN)
 
-async def main():
+async def update_loop():
+  bot = Bot(token=TOKEN)
   hedef_tarih = datetime.datetime(2026, 8, 25, 0, 0, 0)
 
   while True:
@@ -57,8 +62,10 @@ async def main():
           '  🔴 Meşgul Hesaplar\n'
           '└───────────────────┘\n\n'
           f'  Hesap 6 - {kalan_sure} kaldı.\n\n'
-          f'⏱ Son Güncelleme: {datetime.datetime.now().strftime("%d.%m.%Y %H:%M:%S")}\n\n'
-          "Hesap no'ların üzerine tıklayarak hesaplara hızlı bir şekilde ulaşabilirsiniz.\n\n"
+          f'⏱ Son Güncelleme:'
+          f' {datetime.datetime.now().strftime("%d.%m.%Y %H:%M:%S")}\n\n'
+          "Hesap no'ların üzerine tıklayarak hesaplara hızlı bir şekilde"
+          ' ulaşabilirsiniz.\n\n'
           'Hemen kiralamak için;\n'
           '✅ @btkabus'
       )
@@ -66,12 +73,12 @@ async def main():
       await bot.edit_message_text(
           chat_id=CHAT_ID, message_id=MESSAGE_ID, text=metin
       )
-
     except Exception as e:
-      print(f'Hata: {e}')
+      print(f'Hata olustu: {e}')
 
     await asyncio.sleep(5)
 
+
 if __name__ == '__main__':
   keep_alive()
-  asyncio.run(main())
+  asyncio.run(update_loop())
