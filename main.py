9e96e14689356cc8a88b929297331599bb0f90fe
tbtbtahabaykal123-hash <tb.tb.tahabaykal123@gmail.com>
2026-08-20 08:16:28 +0300

    Create main.py

---
+++ main.py
@@ -0,0 +1,39 @@
+import asyncio
+import datetime
+from telegram import Bot
+
+TOKEN = "8978792663:AAFEmc5qriY8a4tuX05yxzARfEr5KgwIMMO"
+CHAT_ID = "@kabusxkira"
+MESSAGE_ID = 4
+
+bot = Bot(token=TOKEN)
+
+async def main():
+    bitis_zamani = datetime.datetime.now() + datetime.timedelta(days=7, hours=22)
+
+    while True:
+        try:
+            kalan = bitis_zamani - datetime.datetime.now()
+            gun = kalan.days
+            saat, artan = divmod(kalan.seconds, 3600)
+            dakika, saniye = divmod(artan, 60)
+
+            metin = (
+                f"🟢 **Müsait Hesaplar**\nHesap 6\n\n"
+                f"🔴 **Meşgul Hesaplar**\n"
+                f"Hesap 1 - {gun}g, {saat}s, {dakika}d, {saniye}s kaldı.\n\n"
+                f"🕒 *Son Güncelleme:* {datetime.datetime.now().strftime('%d.%m.%Y %H:%M:%S')}"
+            )
+
+            await bot.edit_message_text(
+                chat_id=CHAT_ID,
+                message_id=MESSAGE_ID,
+                text=metin,
+                parse_mode="Markdown"
+            )
+        except Exception as e:
+            print(f"Hata: {e}")
+            
+        await asyncio.sleep(5)
+
+asyncio.run(main())
